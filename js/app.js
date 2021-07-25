bd54d787b3662d466e1c23b318b343fa4ab1ea54
DittyCode <pluzekq@wp.pl>
2021-07-25 13:08:08 +0200

    Functional for hamburger, make a message information and email validator

--- js/app.js
+++ js/app.js
@@ -38,10 +38,48 @@ progressBar(progress[0]);
 // <<<<<<<<<<<<< HAMBURGER >>>>>>>>>>>>>>>>
 
 const hamburger = document.querySelector('[data-hamburger]');
+const iconHamburger = document.querySelector('[data-hamburgerImg]');
 const nav = document.querySelector('.nav');
 
 const dropMenu = (e) => {
 	nav.classList.toggle('drop');
+	if (nav.classList.contains('drop')) {
+		iconHamburger.setAttribute('src', './../images/icon-close.svg');
+		console.log(hamburger.getAttribute('src'));
+	} else {
+		iconHamburger.setAttribute('src', './../images/icon-hamburger.svg');
+	}
 };
 
 hamburger.addEventListener('click', (e) => dropMenu(e));
+
+// <<<<<<<<<<<<< Email >>>>>>>>>>>>>
+
+const input = document.querySelector('[data-emailInput]');
+const sendEmail = document.querySelector('[data-emailSend]');
+const message = document.querySelector('[data-sendMessage]');
+
+const checkValidEmail = (email) => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email.toLowerCase());
+};
+
+const clearMessage = () => setTimeout(() => (message.textContent = ''), 3000);
+
+const sendPersonalEmail = () => {
+	const value = input.value;
+	if (value) {
+		const validate = checkValidEmail(value);
+		if (validate) {
+			message.textContent = 'Message send into Email';
+		} else {
+			message.textContent = ' Remember about valid email';
+		}
+	} else {
+		message.textContent = "Input can't be empty.";
+	}
+	clearMessage();
+};
+
+sendEmail.addEventListener('click', sendPersonalEmail);
